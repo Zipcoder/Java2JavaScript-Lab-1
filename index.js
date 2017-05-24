225770a0071ca34f11a3d796c91fed08c5adeecc
Aaron Long <aaron.long.c@gmail.com>
2017-05-24 16:56:20 -0400

    Did Square Numbers

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+'use strict'
 import { loops } from './js/loops'
+import { oddNumbers } from './js/oddNumbers'
+import { squareNumbers } from './js/squareNumbers'
 
 function main () {
-  console.log('1 to 10\n')
+  // Loops
+  console.log('1 to 10')
   loops()
+
+  // Odd Numbers
+  console.log('\nOdd Numbers')
+  oddNumbers()
+
+  // Odd Numbers
+  console.log('\nSquare Numbers')
+  squareNumbers()
 }
 
 main()
