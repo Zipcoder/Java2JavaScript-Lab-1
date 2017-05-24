e0a2d442cfe2b98f210e5347aef01a2aa6664d0f
Molly Arant <mccormick.mollyt@gmail.com>
2017-05-24 18:47:03 -0400

    added in functions for things up to triangle

--- js/script.js
+++ js/script.js
@@ -5,3 +5,67 @@ var display = document.getElementById("display");
 function yourFunctionName (){
   display.innerHTML = "hello";
 }
+
+function printNumbersOneToTen(){
+  display.innerHTML=("oneToTen<br>***Output***");
+  for(var i=1;i<=10;i++){
+    display.innerHTML+=("<br>" +i);
+  }
+}
+function odd(){
+  display.innerHTML=("oddNumbers()<br>***Output***");
+  for(var i=1;i<20;i+=2){
+    display.innerHTML+=("<br>"+i);
+  }
+}
+function squareNumbersto100(){
+  display.innerHTML=("squares()<br>***Output***");
+  for(var i=1;i<=10;i++){
+    display.innerHTML+=("<br>"+ i*i);
+  }
+}
+
+function randomNumberGenerator(){
+display.innerHTML=("random4()<br>***Output***");
+  for(var i=0;i<4;i++){
+    var randomNumber= Math.floor(Math.random()*100);
+    display.innerHTML+=("<br>"+randomNumber);
+  }
+}
+
+function even(n){
+  display.innerHTML=("even(20)<br>***Output***");
+  for(var i=2;i<n;i+=2){
+    display.innerHTML+=("<br>"+i);
+  }
+}
+
+function powersOfTwo(n){
+  display.innerHTML=("***Output***");
+  for(var i=1;i<=n;i++){
+    display.innerHTML+="<br>" + Math.pow(2, i);
+  }
+}
+
+function areWeThereYet(){
+  var answer = prompt("Are we there yet?");
+
+while(answer!="yes"){
+  answer= prompt("Are we there yet?");
+
+}
+display.innerHTML="Good!";
+}
+
+function triangle(){
+  var filledString="triangle()<br>***Output***";
+  for(var i=1;i<=6;i++){
+    for(var j=1;j<i;j++){
+      filledString+=("*");
+    }
+    if(i<6){
+      filledString+=("<br>");
+    }
+  }
+  display.innerHTML=filledString;
+}
