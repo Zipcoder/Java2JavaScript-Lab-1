43a38a3c833735983e33ecdaa202d74d7f84487a
Dennis Kalaygian <daeniskalaygian@gmail.com>
2017-05-24 16:41:39 -0400

    2.1

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+function oneToTen (){
+  for(i = 1; i < 11; i++){
+      display.innerHTML += i + "<br/>";
+  }
+        
 }
